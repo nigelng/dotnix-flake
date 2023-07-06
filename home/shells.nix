f055a795e86718f0d2dd097808053012aa83f294
Nigel Nguyen <nigel@nng.me>
2023-07-07 09:31:03 +1000

    set catppucin theme for btop & fzf

--- home/shells.nix
+++ home/shells.nix
@@ -11,6 +11,20 @@
       enable = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
+      colors = {
+        "bg+" = "#313244";
+        bg = "#1e1e2e";
+        fg = "#cdd6f4";
+        "fg+" = "#cdd6f4";
+        hl = "#f38ba8";
+        "hl+" = "#f38ba8";
+        spinner = "#f5e0dc";
+        header = "#f38ba8";
+        info = "#cba6f7";
+        pointer = "#f5e0dc";
+        marker = "#f5e0dc";
+        prompt = "#cba6f7";
+      };
     };
     zoxide = {
       enable = true;
